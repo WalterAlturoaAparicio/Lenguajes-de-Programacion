4530b4228f0064a8a91c531230db617d3d4b6ed2
atthosar <walturoa@academia.usbbog.edu.co>
2020-05-20 17:10:18 -0500

    no message

---
+++ python/clase1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 20:43:46 2020
+python
+@author: walter
+"""
+import random
+n=[1,212,21,21]
+p="Rusia"
+def myfunc():
+    print("el pais mas grande del mundo "+p)
+
+y=range(4)
+d=b"73821"
+x= 3.99
+r=""" padre nuestro padre nuestro padre nuestro 
+    padre nuestro padre nuestro padre nuestro 
+    padre nuestro padre nuestro padre nuestro  """
+s=b"gjiewjgo"
+num = random.randint(1,213)
+w="Walter Alturo Aparicio"
+print(w[0])
+print(w[7])
+print(w[14])
